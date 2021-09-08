277abb26494802a058290817a556f96468c75374
carlaberni <64801675+carlaberni@users.noreply.github.com>
2021-09-08 12:38:25 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            deMarque<span className="fancy"> Ateliê de Costura e Modelagem</span>
+            PyLadies<span className="fancy"> São Carlos</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
           <h2>A blog template with Next.js and Netlify.</h2>
